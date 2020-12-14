2ac93d6230424d76171ef02a4b61d5c6501037fd
limjinyung <jinyung0069@hotmail.com>
2020-12-14 15:42:31 +0800

    make some changes to test jenkins

--- helloworld/sms/views.py
+++ helloworld/sms/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from .serializers import StudentSerializer
 from .models import Student
 
-# Create your views here.
 class StudentView(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
